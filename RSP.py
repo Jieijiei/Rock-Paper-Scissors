6cea5f1d8fe138bf28a695ccb34029983920bc2f
wonder_staff <wonder_staff@MAC20170615-6.local>
2019-03-13 21:02:10 +0900

    made file

---
+++ RSP.py
@@ -0,0 +1,9 @@
+import random
+print("じゃんけんをしましょう！！0：グー、2：チョキ、1：パー")
+for i in range(10):
+    com = random.randint(0,2)
+    print("いきますよ、じゃーん...けーん...")
+    user = input('数字を入力してください：')
+    if ((user + com) %2 = 0)
+        print('あいこ')
+        //自分の手引く相手の手足す３割る３の余りで識別する
