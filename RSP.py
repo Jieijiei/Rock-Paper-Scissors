7ce18a0be942df9770cc46a60362be6a6adef5c7
wonder_staff <wonder_staff@MAC20180725-003.local>
2019-04-03 20:55:55 +0900

    じゃんけんの勝敗ができるようになった

--- RSP.py
+++ RSP.py
@@ -1,9 +1,23 @@
 import random
-print("じゃんけんをしましょう！！0：グー、2：チョキ、1：パー")
+print("じゃんけんをしましょう！！0：グー、1：チョキ、2：パー")
+userVictory = 0
+userLose = 0
+Even = 0
 for i in range(10):
     com = random.randint(0,2)
     print("いきますよ、じゃーん...けーん...")
-    user = input('数字を入力してください：')
-    if ((user + com) %2 = 0)
-        print('あいこ')
-        //自分の手引く相手の手足す３割る３の余りで識別する
+    User = input('数字を入力してください：')
+    user = int(User)
+    judge = (com + 3 - user) % 3
+    if judge == 0:
+        print("あいこ。")
+        Even + 1
+    if judge == 1:
+        print("あなたの負け！")
+        userLose + 1
+    if judge == 2:
+        print("あなたの勝ち！！")
+        userVictory + 1
+print(userLose)
+print(userVictory)
+print(Even)
